bcad06f4852d2fa978b2ef0e5c5668ad691581f5
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-06 00:50:55 +0200

    ajout migration pour le seeder

---
+++ Migrations/20230405224631_DataSeeder.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetAnnuaireApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Insertion des sites
+            migrationBuilder.InsertData(
+                table: "Sites",
+                columns: new[] { "Id", "Ville" },
+                values: new object[,]
+                {
+            { 1, "Paris" },
+            { 2, "Lyon" },
+            { 3, "Marseille" }
+                });
+
+            // Insertion des services
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Id", "Nom" },
+                values: new object[,]
+                {
+            { 1, "Service informatique" },
+            { 2, "Service commercial" },
+            { 3, "Service marketing" }
+                });
+
+            // Insertion des salariés
+            migrationBuilder.InsertData(
+                table: "Salaries",
+                columns: new[] { "Id", "Nom", "Prenom", "TelephoneFixe", "TelephonePortable", "Email", "ServiceId", "SiteId" },
+                values: new object[,]
+                {
+            { 1, "Dupont", "Jean", "0102030405", "0607080910", "jean.dupont@example.com", 1, 1 },
+            { 2, "Durand", "Pierre", "0102030405", "0607080910", "pierre.durand@example.com", 2, 2 },
+            { 3, "Martin", "Sophie", "0102030405", "0607080910", "sophie.martin@example.com", 3, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Suppression des salariés
+            migrationBuilder.DeleteData(
+                table: "Salaries",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3 });
+
+            // Suppression des services
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3 });
+
+            // Suppression des sites
+            migrationBuilder.DeleteData(
+                table: "Sites",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3 });
+        }
+
+    }
+}
