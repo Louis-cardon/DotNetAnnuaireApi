b35beff56e13f64e0e43539e62264f2a1ca6286a
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-07 01:46:53 +0200

    Ajout service Salarie

---
+++ Services/ISalarieService.cs
@@ -0,0 +1,18 @@
+﻿using DotNetAnnuaireApi.Models;
+
+namespace DotNetAnnuaireApi.Services
+{
+    public interface ISalarieService
+    {
+        Task<IEnumerable<Salarie>> GetAllSalariesAsync();
+        Task<Salarie> GetSalarieByIdAsync(int id);
+        Task<Salarie> AddSalarieAsync(Salarie Salarie);
+        Task UpdateSalarieAsync(Salarie Salarie);
+        Task DeleteSalarieAsync(int id);
+
+
+        Task<IEnumerable<Salarie>> GetSalarieByNomAsync(string nom);
+        Task<IEnumerable<Salarie>> GetSalarieBySiteAsync(int siteId);
+        Task<IEnumerable<Salarie>> GetSalarieByServiceAsync(int serviceId);
+    }
+}
---
+++ Services/SalarieService.cs
@@ -0,0 +1,65 @@
+﻿using DotNetAnnuaireApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DotNetAnnuaireApi.Services
+{
+    public class SalarieService : ISalarieService
+    {
+        private readonly AnnuaireContext _context;
+
+        public SalarieService(AnnuaireContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Salarie>> GetAllSalariesAsync()
+        {
+            return await _context.Salaries.ToListAsync();
+        }
+
+        public async Task<Salarie> GetSalarieByIdAsync(int id)
+        {
+            return await _context.Salaries.FindAsync(id);
+        }
+
+        public async Task<Salarie> AddSalarieAsync(Salarie Salarie)
+        {
+            _context.Salaries.Add(Salarie);
+            await _context.SaveChangesAsync();
+            return Salarie;
+        }
+
+        public async Task UpdateSalarieAsync(Salarie Salarie)
+        {
+            _context.Salaries.Update(Salarie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSalarieAsync(int id)
+        {
+            var salarie = await _context.Salaries.FindAsync(id);
+            if (salarie != null)
+            {
+                _context.Salaries.Remove(salarie);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Salarie>> GetSalarieByNomAsync(string nom)
+        {
+            return await _context.Salaries.Where(s => s.Nom.Contains(nom)).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Salarie>> GetSalarieBySiteAsync(int siteId)
+        {
+            return await _context.Salaries.Where(s => s.SiteId == siteId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Salarie>> GetSalarieByServiceAsync(int serviceId)
+        {
+            return await _context.Salaries.Where(s => s.ServiceId == serviceId).ToListAsync();
+        }
+    }
+
+}
