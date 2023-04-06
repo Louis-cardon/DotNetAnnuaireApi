366e8e5d10165b42e1e140aa67c28b56a9235df2
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-07 01:10:27 +0200

    Ajout controller Role

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,84 @@
+﻿namespace DotNetAnnuaireApi.Controllers
+{
+    using System.Collections.Generic;
+    using DotNetAnnuaireApi.Models;
+    using DotNetAnnuaireApi.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Role>>> GetRoles()
+        {
+            var roles = await _roleService.GetAllRolesAsync();
+            return Ok(roles);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Role>> GetRole(int id)
+        {
+            var role = await _roleService.GetRoleByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return Ok(role);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Role>> PostRole(Role role)
+        {
+            if (role == null)
+            {
+                return BadRequest();
+            }
+
+            await _roleService.AddRoleAsync(role);
+            return CreatedAtAction(nameof(GetRole), new { id = role.Id }, role);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRole(int id, Role role)
+        {
+            if (role == null || id != role.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingRole = await _roleService.GetRoleByIdAsync(id);
+
+            if (existingRole == null)
+            {
+                return NotFound();
+            }
+
+            await _roleService.UpdateRoleAsync(role);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var role = await _roleService.GetRoleByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            await _roleService.DeleteRoleAsync(id);
+
+            return NoContent();
+        }
+    }
+}
