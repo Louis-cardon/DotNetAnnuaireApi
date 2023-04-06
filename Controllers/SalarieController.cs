6aaec434c41edc87a9e0cc072395a6f2e5d68e82
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-07 01:46:35 +0200

    Ajout controller Salarie

---
+++ Controllers/SalarieController.cs
@@ -0,0 +1,128 @@
+﻿using DotNetAnnuaireApi.Models;
+using DotNetAnnuaireApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetAnnuaireApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SalarieController : ControllerBase
+    {
+        private readonly ISalarieService _salarieService;
+
+        public SalarieController(ISalarieService salarieService)
+        {
+            _salarieService = salarieService;
+        }
+
+        // GET: api/Salarie
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Salarie>>> GetSalaries()
+        {
+            var result = await _salarieService.GetAllSalariesAsync();
+            return Ok(result);
+        }
+
+        // GET: api/Salarie/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Salarie>> GetSalarie(int id)
+        {
+            var salarie = await _salarieService.GetSalarieByIdAsync(id);
+
+            if (salarie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(salarie);
+        }
+
+        // POST: api/Salarie
+        [HttpPost]
+        public async Task<ActionResult<Salarie>> PostSalarie([FromBody] Salarie salarie)
+        {
+            if (salarie == null)
+            {
+                return BadRequest();
+            }
+
+            await _salarieService.AddSalarieAsync(salarie);
+
+            return CreatedAtAction(nameof(GetSalarie), new { id = salarie.Id }, salarie);
+        }
+
+        // PUT: api/Salarie/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSalarie(int id, [FromBody] Salarie salarie)
+        {
+            if (salarie == null || id != salarie.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingSalarie = await _salarieService.GetSalarieByIdAsync(id);
+
+            if (existingSalarie == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await _salarieService.UpdateSalarieAsync(salarie);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Salarie/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSalarie(int id)
+        {
+
+            var salarie = await _salarieService.GetSalarieByIdAsync(id);
+
+            if (salarie == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await _salarieService.DeleteSalarieAsync(id);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet("nom/{nom}")]
+        public async Task<ActionResult<IEnumerable<Salarie>>> GetSalarieByNomAsync(string nom)
+        {
+            var salaries = await _salarieService.GetSalarieByNomAsync(nom);
+            return Ok(salaries);
+        }
+
+        [HttpGet("site/{siteId}")]
+        public async Task<ActionResult<IEnumerable<Salarie>>> GetSalarieBySiteAsync(int siteId)
+        {
+            var salaries = await _salarieService.GetSalarieBySiteAsync(siteId);
+            return Ok(salaries);
+        }
+
+        [HttpGet("service/{serviceId}")]
+        public async Task<ActionResult<IEnumerable<Salarie>>> GetSalarieByServiceAsync(int serviceId)
+        {
+            var salaries = await _salarieService.GetSalarieByServiceAsync(serviceId);
+            return Ok(salaries);
+        }
+    }
+}
