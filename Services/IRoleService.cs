8b52b79bb7c2741dda4edc8da4b83e9abb8a68c5
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-07 01:10:50 +0200

    Ajout Service Role Async

---
+++ Services/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using DotNetAnnuaireApi.Models;
+
+namespace DotNetAnnuaireApi.Services
+{
+    public interface IRoleService
+    {
+        Task<List<Role>> GetAllRolesAsync();
+        Task<Role> GetRoleByIdAsync(int id);
+        Task<Role> AddRoleAsync(Role role);
+        Task UpdateRoleAsync(Role role);
+        Task DeleteRoleAsync(int id);
+    }
+}
