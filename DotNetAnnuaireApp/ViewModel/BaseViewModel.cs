10a01e9795d224e3574e6a61391a3ebfb1ba4128
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-20 01:03:00 +0200

    ajout BaseViewModel

---
+++ DotNetAnnuaireApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAnnuaireApp.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+
+    public BaseViewModel() 
+    {
+    
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !isBusy;
+
+
+}
