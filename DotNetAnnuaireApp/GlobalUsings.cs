a0ee8d715bd0b25130814bcb8697019e5ba26a73
Louis-alt <cardonlouis@orange.fr>
2023-04-23 19:36:35 +0200

    ajout des usings

--- DotNetAnnuaireApp/GlobalUsings.cs
+++ DotNetAnnuaireApp/GlobalUsings.cs
@@ -1,1 +1,8 @@
 ﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using System.Collections.ObjectModel;
+global using DotNetAnnuaireApp.ViewModel;
+global using CommunityToolkit.Mvvm.Input;
+global using DotNetAnnuaireApp.Services;
+global using DotNetAnnuaireApp.Models;
+global using DotNetAnnuaireApp.View;
+global using Newtonsoft.Json;
