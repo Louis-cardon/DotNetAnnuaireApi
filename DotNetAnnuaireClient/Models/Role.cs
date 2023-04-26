dbdb9a293d2ef8fafa7cf9f3e66ea53608d2c3ca
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-04-26 14:29:54 +0200

    ajout 4 model

---
+++ DotNetAnnuaireClient/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAnnuaireClient.Models;
+
+public class Role
+{
+    public int Id { get; set; } // identifiant unique du rôle
+    public string Nom { get; set; } // nom du rôle
+    public ICollection<Salarie>? Salaries { get; set; } // liste des salariés ayant ce rôle
+}
---
+++ DotNetAnnuaireClient/Models/Site.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAnnuaireClient.Models;
+
+public class Site
+{
+    public int Id { get; set; } // identifiant unique du site
+    public string Ville { get; set; } // ville du site
+    public ICollection<Salarie>? Salaries { get; set; }
+}
